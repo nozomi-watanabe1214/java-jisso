e60c23cee2a377512b71d39ef7d506d594d5a18d
渡辺のぞみ <@192.168.0.15>
2025-03-25 21:52:25 +0900

    Javaオブジェクト指向・実装（演習⑦完了）

--- src/iphone2/Iphone.java
+++ src/iphone2/Iphone.java
@@ -5,11 +5,14 @@ public class Iphone {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		SmartPhone iphone = new SmartPhone();
+		SmartPhone2 iphone = new SmartPhone2();
 		iphone.play();
 		iphone.stop();
 		iphone.next();
 		iphone.back();
+		iphone.call();
+		iphone.mail();
+		iphone.photo();
+		iphone.internet();
+		}
 	}
-
-}
